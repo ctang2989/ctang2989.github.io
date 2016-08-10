b177a3c7e1ec31b58243c5149ce52afb436be699
Carmen Tang <ctang2989@bths.edu>
2016-08-10 15:47:37 -0400

    panda javascript

---
+++ warning.js
@@ -0,0 +1,8 @@
+function myFunction(){
+	alert('WARNING! THIS IS AN EMERGENCY! YOU JUST GOT PRANKED');
+}
+
+
+function switcheroo(){
+	document.getElementById("types").innerHTML = "Random Pandas";
+}
